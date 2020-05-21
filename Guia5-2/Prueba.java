deb1e402b8476d7b20a42c75991352868e1fa752
Carlos Enrique Enrique <cemontenegrom@udistrital.edu.co>
2020-05-21 21:54:41 +0000

    Identacion de Guia 5-2

--- Guia5-2/Prueba.java
+++ Guia5-2/Prueba.java
@@ -1,11 +1,12 @@
 public class Prueba {
-public static void main (String args[]){
-char i ='2';
-switch (i) {
- case '1': System.out.println( "i contiene un 1"); break;
- case '2': System.out.println( "i contiene un 2"); break;
- case '3': System.out.println( "i contiene un 3"); break;
- default : System.out.println( "i no es 1, 2 ó 3 ");
-}
-}
+
+  public static void main (String args[]){
+    char i ='2';
+    switch (i) {
+      case '1': System.out.println( "i contiene un 1"); break;
+      case '2': System.out.println( "i contiene un 2"); break;
+      case '3': System.out.println( "i contiene un 3"); break;
+      default : System.out.println( "i no es 1, 2 ó 3 ");
+    }
+  }
 }
