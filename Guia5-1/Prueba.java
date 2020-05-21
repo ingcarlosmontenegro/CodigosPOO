c56726c51c63c714fd7c92b86c70d42558bf9a1e
Carlos Enrique Enrique <cemontenegrom@udistrital.edu.co>
2020-05-21 19:37:09 +0000

    se agrego 5-1

---
+++ Guia5-1/Prueba.java
@@ -0,0 +1,11 @@
+public class Prueba {
+public static void main (String args[]){
+int i=5;
+if (i == 5){ System.out.println(" i vale 5 ");}
+else {System.out.println("i no vale 5");}
+i=4;
+if (i == 5){ System.out.println(" i vale 5 ");}
+else if (i < 5){System.out.println("i es menor que 5");}
+else if (i > 5){System.out.println("i es mayor que 5");}
+}
+}
