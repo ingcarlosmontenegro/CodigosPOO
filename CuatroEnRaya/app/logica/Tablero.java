9e25fdb19a9ae797aab9d35746aef2b7ec820812
repl.it user <replituser@example.com>
2020-06-17 03:47:22 +0000

    mejoras al triqui

--- CuatroEnRaya/app/logica/Tablero.java
+++ CuatroEnRaya/app/logica/Tablero.java
@@ -3,6 +3,14 @@ package app.logica;
 public class Tablero{
   private Ficha tableroJuego[][] = new Ficha [3][3];
 
+  public Ficha[][] getTablero (){
+    return tableroJuego;
+  }
+
+  public void setTablero (Ficha tableroTemp[][]){
+    this.tableroJuego = tableroTemp;
+  }
+
   public void llenar(){
     for (int i=0; i<3; i++){
       for (int j=0; j<3; j++){
@@ -19,6 +27,46 @@ public class Tablero{
       }
       System.out.println("");  
     }
+  }
+
+  public boolean verificarVacio (int x, int y){
+    if (tableroJuego[x][y].getForma().equals("-")){
+      return true;
+    }
+    else
+     return false;
+  }
+
+  public boolean validarGanador(){
+     boolean bandera;
+     bandera = validarFilas();  
+     if (!bandera)
+      bandera = validarColumnas();
+     return bandera;
+  }
+  
+  public boolean validarFilas(){
+    for (int i= 0; i < 3; i++){
+        if ((tableroJuego[i][0].getForma().equals("X") && tableroJuego[i][1].getForma().equals("X") && tableroJuego[i][2].getForma().equals("X"))
+        ||
+        (tableroJuego[i][0].getForma().equals("O") && tableroJuego[i][1].getForma().equals("O") && tableroJuego[i][2].getForma().equals("O")))
+        {
+          return true;
+        }
+    }
+      return false;
+  }
+
+  public boolean validarColumnas(){
+    for (int i= 0; i < 3; i++){
+        if ((tableroJuego[0][i].getForma().equals("X") && tableroJuego[1][i].getForma().equals("X") && tableroJuego[2][i].getForma().equals("X"))
+        ||
+        (tableroJuego[0][i].getForma().equals("O") && tableroJuego[1][i].getForma().equals("O") && tableroJuego[2][i].getForma().equals("O")))
+        {
+          return true;
+        }
+    }
+      return false;
   }
 
 }
