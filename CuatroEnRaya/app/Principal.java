ab419427f70a8c55cb08b046001c0a59927085e8
repl.it user <replituser@example.com>
2020-10-05 21:58:13 +0000

    cambios en b/CuatroEnRaya/app/Principal.java

--- CuatroEnRaya/app/Principal.java
+++ CuatroEnRaya/app/Principal.java
@@ -1,6 +1,7 @@
 package app;
 
 import app.logica.*;
+import java.util.Scanner;
 
 /**
  * @author carlosmontenegromarin
@@ -8,12 +9,55 @@ import app.logica.*;
  */
 public class Principal{
 
-  private Tablero juego = new Tablero();
+ Scanner sc = new Scanner (System.in);
+
+ Tablero juego = new Tablero();
 
  public static void main (String args[]){
    Principal inicio = new Principal();
    inicio.juego.llenar();
    inicio.juego.ver();
+   inicio.jugar();
  }  
+
+ void jugar(){
+  int x, y;
+  int turno=1;
+  while (turno <= 9){
+    x = sc.nextInt();
+    y = sc.nextInt();
+    if (x>=0 && x<=2 && y>=0 && y<=2){
+      if (turno%2!=0){
+        if (juego.verificarVacio(x, y)){
+          juego.getTablero()[x][y].setForma("X");
+          juego.ver();
+          if (juego.validarGanador()){
+          System.out.println("Hay ganador X");
+          turno=10;
+          }
+          turno++;
+        }
+      }
+      else {
+        if (juego.verificarVacio(x, y)){
+          juego.getTablero()[x][y].setForma("O");
+          juego.ver();
+           if (juego.validarGanador()){
+            System.out.println("Hay ganador O");
+            turno=10;
+          }
+          turno++;
+        }   
+      }
+    }
+    else
+     System.out.println("x o y estan fuera de rango");
+  }  
+}
+
+
+
+
+
 
 }
