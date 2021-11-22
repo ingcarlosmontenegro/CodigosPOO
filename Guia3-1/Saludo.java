d395562deb4f5386a1c86c9a37f9be05a6cd9e70
CarlosEnriqueE7 <>
2021-11-22 16:04:08 +0000

    Modificacion ejercicio Gua 3-1

--- Guia3-1/Saludo.java
+++ Guia3-1/Saludo.java
@@ -5,15 +5,15 @@ import javax.swing.JFrame;
 public class Saludo extends JApplet {
 
   public void paint (Graphics g) {
-    g.drawString ("Hola", 50, 50);
+    g.drawString("Hola Mundo", 50, 50);
   }
 
   public static void main (String[] args){
-    JFrame frame = new JFrame ("Cargando JApplet");
+    JFrame frame = new JFrame ("Mostrando JApplet");
     Saludo saludoApplet = new Saludo();
-    saludoApplet.init();
+    //saludoApplet.init();
     frame.getContentPane().add(saludoApplet);
-    frame.setSize(200,200);
+    frame.setSize(300,300);
     frame.setVisible(true);
   }
 }
